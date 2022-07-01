36269a23d6b0ca39e5b3d48534aebea3750b88c1
tugalsan karabacak <tugalsan@gmail.com>
2022-07-01 09:54:50 +0300

    usage of TGS_UnSafe class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.tugalsan.api.file.zip7 {
     requires sevenzipjbinding;
     requires org.apache.commons.compress;
     requires com.tugalsan.api.os;
+    requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.bytes;
     exports com.tugalsan.api.file.zip7.server;
 }
