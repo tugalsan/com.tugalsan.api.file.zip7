eeb5b79c4c5cc89a8db3a3bee2971ac494d58e04
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-15 12:06:19 +0300

    .

--- src/main/java/com/tugalsan/api/file/zip7/server/TS_FileZip7Utils.java
+++ src/main/java/com/tugalsan/api/file/zip7/server/TS_FileZip7Utils.java
@@ -1,26 +1,32 @@
 package com.tugalsan.api.file.zip7.server;
 
-import com.tugalsan.api.unsafe.client.*;
+import com.tugalsan.api.union.client.TGS_UnionExcuseVoid;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.util.*;
+import java.io.IOException;
 import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
 import org.apache.commons.compress.archivers.sevenz.SevenZFile;
 import org.apache.commons.compress.archivers.sevenz.SevenZOutputFile;
 
 public class TS_FileZip7Utils {
 
-    public static void compress(CharSequence name, File... files) {
-        TGS_UnSafe.run(() -> {
-            try ( var out = new SevenZOutputFile(new File(name.toString()))) {
-                Arrays.stream(files).forEachOrdered(file -> addToArchiveCompression(out, file, "."));
+    public static TGS_UnionExcuseVoid compress(CharSequence name, File... files) {
+        try (var out = new SevenZOutputFile(new File(name.toString()))) {
+            for (var file : files) {
+                var u = addToArchiveCompression(out, file, ".");
+                if (u.isExcuse()) {
+                    return u;
+                }
             }
-        });
+            return TGS_UnionExcuseVoid.ofVoid();
+        } catch (IOException ex) {
+            return TGS_UnionExcuseVoid.ofExcuse(ex);
+        }
     }
 
-    public static void decompress(CharSequence in, File destination) {
-        TGS_UnSafe.run(() -> {
+    public static TGS_UnionExcuseVoid decompress(CharSequence in, File destination) {
+        try {
             var sevenZFile = new SevenZFile(new File(in.toString()));
             SevenZArchiveEntry entry;
             while ((entry = sevenZFile.getNextEntry()) != null) {
@@ -34,20 +40,23 @@ public class TS_FileZip7Utils {
                 }
                 var content = new byte[(int) entry.getSize()];
                 sevenZFile.read(content, 0, content.length);
-                try ( var out = new FileOutputStream(curfile)) {
+                try (var out = new FileOutputStream(curfile)) {
                     out.write(content);
                 }
             }
-        });
+            return TGS_UnionExcuseVoid.ofVoid();
+        } catch (IOException ex) {
+            return TGS_UnionExcuseVoid.ofExcuse(ex);
+        }
     }
 
-    private static void addToArchiveCompression(SevenZOutputFile out, File file, CharSequence dir) {
-        TGS_UnSafe.run(() -> {
+    private static TGS_UnionExcuseVoid addToArchiveCompression(SevenZOutputFile out, File file, CharSequence dir) {
+        try {
             var name = dir + File.separator + file.getName();
             if (file.isFile()) {
                 var entry = out.createArchiveEntry(file, name);
                 out.putArchiveEntry(entry);
-                try ( var in = new FileInputStream(file);) {
+                try (var in = new FileInputStream(file);) {
                     var b = new byte[1024];
                     int count;
                     while ((count = in.read(b)) > 0) {
@@ -65,6 +74,9 @@ public class TS_FileZip7Utils {
             } else {
                 System.out.println(file.getName() + " is not supported");
             }
-        });
+            return TGS_UnionExcuseVoid.ofVoid();
+        } catch (IOException ex) {
+            return TGS_UnionExcuseVoid.ofExcuse(ex);
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.tugalsan.api.file.zip7 {
     requires sevenzipjbinding;
     requires org.apache.commons.compress;
     requires com.tugalsan.api.os;
-    requires com.tugalsan.api.unsafe;
+    requires com.tugalsan.api.union;
     requires com.tugalsan.api.bytes;
     exports com.tugalsan.api.file.zip7.server;
 }
